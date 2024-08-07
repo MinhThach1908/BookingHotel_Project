fa05e6d86601ba546d32dbb3e7f48547eb4843c1
MinhThach1908 <pandaminh2008@gmail.com>
2024-08-07 11:27:49 +0700

    Service + Seeder Customer

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'full_name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => $this->faker->password(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'photo' => $this->faker->imageUrl(),
+        ];
+    }
+}
---
+++ database/factories/RoomtypeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\RoomType>
+ */
+class RoomtypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title'=>$this->faker->word(),
+            'detail'=>$this->faker->text(),
+            'price'=>$this->faker->randomFloat(2,1000,10000),
+        ];
+    }
+}
